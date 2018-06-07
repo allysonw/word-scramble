5591da849e4c369453b853dd6c62d9ce0c31f458
Allyson Wesman <allysonwesman@gmail.com>
2018-06-07 16:35:13 -0700

    move logic for words api call to a service object

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Api::V1::GamesController < ApplicationController
   def index
     games = Game.all
@@ -7,34 +5,15 @@ class Api::V1::GamesController < ApplicationController
   end
 
   def create
-    body_hash = dictionary_request
+    words_api = WordsApiService.new
 
-    word = body_hash['word']
-    definition = body_hash['results'][0]['definition']
+    words_array = words_api.getWords
 
     game = Game.create(complete: false)
-    game.words.create(letters: word, difficulty: 0, definition: definition)
+    game.words = words_array
 
     render json: game
   end
 
-  def dictionary_request
-    begin
-      @resp = Faraday.get 'https://wordsapiv1.p.mashape.com/words?hasDetails=definitions&limit=5&lettersMin=5&lettersMax=10&random=true' do |req|
-        req.headers['X-Mashape-Key'] = 'cXvZHDBJCzmshtUAofR5uVwNwswip14MNyEjsn8SyBC8VbDKrY'
-        req.headers['Accept'] = 'application/json'
-      end
 
-      body_hash = JSON.parse(@resp.body)
-
-      if @resp.success?
-        @resp
-      else
-        @error = 'error!'
-      end
-
-    rescue Faraday::ConnectionFailed
-        @error = "There was a timeout. Please try again."
-    end
-  end
 end
---
+++ app/services/words_api_service.rb
@@ -0,0 +1,36 @@
+class WordsApiService
+  # returns an array of new Word objects based on words from WordsAPI
+  def getWords
+    words_array = [];
+
+    while words_array.length < 5 do
+      begin
+        @resp = Faraday.get 'https://wordsapiv1.p.mashape.com/words?hasDetails=definitions&limit=5&lettersMin=5&lettersMax=10&random=true' do |req|
+          req.headers['X-Mashape-Key'] = ENV['MASHAPE_KEY']
+          req.headers['Accept'] = 'application/json'
+        end
+
+        body_hash = JSON.parse(@resp.body)
+
+      rescue Faraday::ConnectionFailed
+          @error = "There was a timeout. Please try again."
+      end
+
+      word = body_hash['word']
+      definition = body_hash['results'][0]['definition']
+      frequency = body_hash['frequency']
+
+      # TODO: figure out a faster way to do this with a frequency above 2.5
+      if frequency != nil && frequency > 0
+        new_word = Word.create(letters: word, difficulty: 0, definition: definition)
+
+        words_array.push(new_word);
+        puts "ADDED A NEW WORD WITH FREQUENCY OF #{frequency}"
+        puts "Words array is now #{words_array}"
+
+      end
+    end
+
+    words_array;
+  end
+end
