e5fc732b4e1691372db11707be1fddbd9437af93
Allyson Wesman <allysonwesman@gmail.com>
2018-06-22 09:59:37 -0700

    don't display 0 scores on high scores page

--- rails-app/app/models/score.rb
+++ rails-app/app/models/score.rb
@@ -2,6 +2,6 @@ class Score < ApplicationRecord
   belongs_to :game
 
   def self.top_x_scores(x)
-    order(value: :desc).limit(x)
+    where('value > ?', 0).order(value: :desc).limit(x)
   end
 end
